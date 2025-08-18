a48a66f40dadebccee3b9d73d9beb10b59ee195b
Samuel Hellstrøm <hellstromsamuel@gmail.com>
2025-08-18 13:19:54 +0200

    Integrate LogoutButton into Header component, displaying it conditionally based on user session status

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,11 +1,14 @@
+import useAuthSession from "@/hooks/useAuthSession";
 import { cn } from "../../lib/utils";
 import ThemeToggleButtons from "../theme/ThemeToggleButtons";
+import LogoutButton from "./LogoutButton";
 
 interface Props {
   className?: string;
 }
 
 function Header({ className }: Props) {
+  const { session } = useAuthSession();
   return (
     <div
       className={cn(
@@ -15,8 +18,11 @@ function Header({ className }: Props) {
     >
       <h1 className="font-semibold text-xl">Hidden Gems</h1>
 
-      <div className="border rounded-xl p-0.5">
-        <ThemeToggleButtons size="sm" />
+      <div className="flex items-center gap-2">
+        {session && <LogoutButton />}
+        <div className="border rounded-xl p-0.5">
+          <ThemeToggleButtons size="sm" />
+        </div>
       </div>
     </div>
   );
