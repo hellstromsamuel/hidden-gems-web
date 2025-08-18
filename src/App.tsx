595aa719f087c97b7db78ee005174894d7f17c71
Samuel Hellstrøm <hellstromsamuel@gmail.com>
2025-08-18 15:13:44 +0200

    Refactor App component to utilize MainContent, separating location management and enhancing code organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
 import { Container } from "./components/ui/container";
 import AuthEmailForm from "./features/auth/components/AuthEmailForm";
-import LocationForm from "./features/locations/components/LocationForm";
-import SavedLocations from "./features/locations/components/SavedLocations";
 import useTheme from "./hooks/useTheme";
 import useAuthSession from "./hooks/useAuthSession";
 import Header from "./components/layout/Header";
-
-function MainContent() {
-  return (
-    <main className="flex flex-col max-w-5xl mx-auto">
-      <div className="grid md:grid-cols-3 gap-8">
-        <Container className="h-max md:col-span-2">
-          <AuthEmailForm />
-          <h2 className="font-semibold text-lg">Saved locations</h2>
-          <SavedLocations />
-        </Container>
-
-        <Container className="h-max md:sticky md:top-4">
-          <h2 className="font-semibold text-lg">Add location</h2>
-          <LocationForm type="add" />
-        </Container>
-      </div>
-    </main>
-  );
-}
+import MainContent from "./MainContent";
 
 function App() {
   const { theme } = useTheme();
---
+++ src/MainContent.tsx
@@ -0,0 +1,23 @@
+import { Container } from "@/components/ui/container";
+import SavedLocations from "./features/locations/components/SavedLocations";
+import LocationForm from "./features/locations/components/LocationForm";
+
+function MainContent() {
+  return (
+    <main className="flex flex-col max-w-5xl mx-auto">
+      <div className="grid md:grid-cols-3 gap-8">
+        <Container className="h-max md:col-span-2">
+          <h2 className="font-semibold text-lg">Saved locations</h2>
+          <SavedLocations />
+        </Container>
+
+        <Container className="h-max md:sticky md:top-4">
+          <h2 className="font-semibold text-lg">Add location</h2>
+          <LocationForm type="add" />
+        </Container>
+      </div>
+    </main>
+  );
+}
+
+export default MainContent;
