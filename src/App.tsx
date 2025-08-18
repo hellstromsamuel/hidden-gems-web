53b8aa891fdfb2c4333d97f377c698947ce64501
Samuel Hellstrøm <hellstromsamuel@gmail.com>
2025-08-18 13:20:01 +0200

    Refactor App component to include MainContent for authenticated users, integrating AuthEmailForm and updating session handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import Header from "./components/layout/header";
 import { Container } from "./components/ui/container";
+import AuthEmailForm from "./features/auth/components/AuthEmailForm";
 import LocationForm from "./features/locations/components/LocationForm";
 import SavedLocations from "./features/locations/components/SavedLocations";
 import useTheme from "./hooks/useTheme";
-// import useAuthSession from "./hooks/useAuthSession";
+import useAuthSession from "./hooks/useAuthSession";
+import Header from "./components/layout/Header";
+
+function MainContent() {
+  return (
+    <main className="flex flex-col max-w-5xl mx-auto">
+      <div className="grid md:grid-cols-3 gap-8">
+        <Container className="h-max md:col-span-2">
+          <AuthEmailForm />
+          <h2 className="font-semibold text-lg">Saved locations</h2>
+          <SavedLocations />
+        </Container>
+
+        <Container className="h-max md:sticky md:top-4">
+          <h2 className="font-semibold text-lg">Add location</h2>
+          <LocationForm type="add" />
+        </Container>
+      </div>
+    </main>
+  );
+}
 
 function App() {
   const { theme } = useTheme();
-  // const { session } = useAuthSession();
+  const { session } = useAuthSession();
 
   return (
     <div
@@ -18,19 +38,15 @@ function App() {
         <Header className="max-w-5xl mx-auto" />
       </div>
 
-      <main className="flex flex-col px-4 py-8 max-w-5xl mx-auto">
-        <div className="grid md:grid-cols-3 gap-8">
-          <Container className="h-max md:col-span-2">
-            <h2 className="font-semibold text-lg">Saved locations</h2>
-            <SavedLocations />
+      <div className="px-4 py-8">
+        {session ? (
+          <MainContent />
+        ) : (
+          <Container className="max-w-sm mx-auto">
+            <AuthEmailForm />
           </Container>
-
-          <Container className="h-max md:sticky md:top-4">
-            <h2 className="font-semibold text-lg">Add location</h2>
-            <LocationForm type="add" />
-          </Container>
-        </div>
-      </main>
+        )}
+      </div>
     </div>
   );
 }
