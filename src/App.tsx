64a33cb22cbd90c38b415195df2712c6cc920a0f
Samuel Hellstrøm <hellstromsamuel@gmail.com>
2025-08-19 12:29:31 +0200

    Add Header component to App for improved layout and user interaction; update App styles for better responsiveness.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import useTheme from "./hooks/useTheme";
 import useAuthSession from "./hooks/useAuthSession";
 import { cn } from "./lib/utils";
 import MapInteractive from "./features/map/components/MapInteractive";
+import Header from "./components/layout/Header";
 
 function App() {
   const { theme } = useTheme();
@@ -15,9 +16,9 @@ function App() {
       className="bg-gray-100 dark:bg-gray-800 min-h-screen min-w-screen relative"
     >
       <div className={cn("left-0 right-0 z-10 p-4", session && "absolute")}>
-        {/* <Header
+        <Header
           className={cn("rounded-xl max-w-xl mx-auto", session && "shadow-md")}
-        /> */}
+        />
       </div>
 
       {session && <MapInteractive />}
