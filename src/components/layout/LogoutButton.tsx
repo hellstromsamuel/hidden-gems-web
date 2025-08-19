4d761a2a9a743a67a11e002009038ef5d1b98a34
Samuel Hellstrøm <hellstromsamuel@gmail.com>
2025-08-19 10:45:55 +0200

    Enhance LogoutButton component with icon and styling for improved user experience

--- src/components/layout/LogoutButton.tsx
+++ src/components/layout/LogoutButton.tsx
@@ -1,11 +1,17 @@
 import { supabase } from "@/config/supabase";
 import { Button } from "../ui/button";
+import { LogOut } from "lucide-react";
 
 function LogoutButton() {
   async function handleLogout() {
     await supabase.auth.signOut();
   }
-  return <Button onClick={handleLogout}>Logout</Button>;
+  return (
+    <Button onClick={handleLogout} variant="outline" className="text-red-500">
+      <LogOut className="size-4" />
+      Logout
+    </Button>
+  );
 }
 
 export default LogoutButton;
