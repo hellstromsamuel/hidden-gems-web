0259199de2fdc5163328d6350b42ef59f2128489
Samuel Hellstrøm <hellstromsamuel@gmail.com>
2025-08-19 12:28:41 +0200

    Enhance LogoutButton component to handle loading state during logout process, improving user feedback and error handling.

--- src/components/layout/LogoutButton.tsx
+++ src/components/layout/LogoutButton.tsx
@@ -1,13 +1,28 @@
 import { supabase } from "@/config/supabase";
 import { Button } from "../ui/button";
 import { LogOut } from "lucide-react";
+import { useState } from "react";
 
 function LogoutButton() {
+  const [isLoading, setIsLoading] = useState(false);
+
   async function handleLogout() {
-    await supabase.auth.signOut();
+    setIsLoading(true);
+    try {
+      await supabase.auth.signOut();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
   }
   return (
-    <Button onClick={handleLogout} variant="outline" className="text-red-500">
+    <Button
+      onClick={handleLogout}
+      variant="outline"
+      className="text-red-500"
+      isLoading={isLoading}
+    >
       <LogOut className="size-4" />
       Logout
     </Button>
